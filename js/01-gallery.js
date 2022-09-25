3b60a23b3d99e0b4c82df8daf39e572332f9b850
Yaroslav Oliinyk <4106677@gmail.com>
2022-09-25 08:43:00 +0300

    task 1 done!

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const galleryItem = galleryItems
+  .map(
+    (item) =>
+      `<div class="gallery__item">
+  <a class="gallery__link" href="${item.preview}" target="_parent" rel="noreferrer noopener">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`
+  )
+  .join("");
+
+console.log(galleryItem);
+
+gallery.insertAdjacentHTML("beforeend", galleryItem);
+
+console.log(gallery);
+
+gallery.addEventListener("click", lightbox);
+
+function lightbox(evt) {
+  evt.preventDefault();
+  if (evt.target === evt.currentTarget) return;
+
+  const currentImage = evt.target;
+
+  //   import * as basicLightbox from "basiclightbox";
+
+  const instance = basicLightbox.create(
+    `<div class="modal">
+      <img src="${currentImage.dataset.source}" />
+    </div>`
+  );
+  instance.show();
+}
